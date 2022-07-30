39a77f89597d66c5a832896628eb0549f0185ae1
triplek24 <karthikeyanrym@gmail.com>
2022-07-30 12:33:29 +0530

    link updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,7 +89,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href=""
+                href="https://github.com/triplek24/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
